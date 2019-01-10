16aa5fad3077404991aeb7527e5b2337a4ef069b
Gabidabach <46558313+Gabidabach@users.noreply.github.com>
2019-01-10 14:13:57 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-http.createServer(function (request, response) {
-
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-
-  response.end('Li213123123123ad\n');
-}).listen(8081);
-
-console.log;
